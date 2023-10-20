23bbb1f38a5f5afed4bda0f3d064b869f4c85fe3
felipinaseosd <130668980+felipinaseosd@users.noreply.github.com>
2023-10-20 10:02:13 -0300

    first commit

---
+++ clasmetodo.php
@@ -0,0 +1,18 @@
+<?php
+Class Pessoa {
+public $nome;
+public $altura;
+public $nascimento;
+public $hobbie;
+// Agora abaixo será criado um método para a classe Pessoa, que trabalhará com as propriedades do objeto. // O método terá o nome Mostrar Pessoa() e basicamente servirá para mostrar os dados do objeto.
+// Lembre-se que método age dentro do escopo da classe.
+public function MostrarPessoa(){
+echo "Nome: ".$this->nome."<br/>";
+echo "Altura: ".$this->altura."<br/>";
+echo "Nascimento: ".$this->nascimento. "<br/>";
+echo "hobbie: ".$this->hobbie."<br/>";
+}
+// Temos um novo conceito apresentado acima.
+// Trata-se da pseudo-variável $this, que serve basicamente para diferenciar as propriedades do objeto de variáveis locais. // Lembre-se, $this referencia o objeto ATUAL e acessa suas propriedades.
+}
+?>
